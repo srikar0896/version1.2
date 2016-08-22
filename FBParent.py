a287f7d6b6a0ca08bb412249337a601d5c319955
srikar.ch <srikar.0896@gmail.com>
2016-08-22 22:20:28 +0530

    Update FBParent.py

--- FBParent.py
+++ FBParent.py
@@ -9,28 +9,30 @@ import threading
 import os
 
 driver = webdriver.PhantomJS()
-#userId = "Email"
+#userId = "srikar.0896@gmail.com"
 start_time = time.time()
-
-driver.get("https://www.facebook.com")
-fbUsername = "Mail Id"
-emailFieldID = ".//*[@id='email']"
-passFieldID = ".//*[@id='pass']"
-loginButtonXPath = ".//input[@value='Log In']"
-flLogoXpath = "(//a[contains(@href, 'logo')])[1]"
-emailFieldElement = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath(emailFieldID))
-passFieldElement = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath(passFieldID))
-loginButtonElement = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath(loginButtonXPath))
-emailFieldElement.click()
-emailFieldElement.clear()
-emailFieldElement.send_keys(fbUsername)
-passFieldElement.click()
-passFieldElement.clear()
-passFieldElement.send_keys("Password")
-loginButtonElement.click()
-WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath(flLogoXpath))
-driver.save_screenshot("yah.png")
-print("\n    Time Executed - " + str(time.time() - start_time))
-
-driver.quit()
+try:
+    driver.get("https://www.facebook.com")
+    emailFieldID = ".//*[@id='email']"
+    passFieldID = ".//*[@id='pass']"
+    loginButtonXPath = ".//input[@value='Log In']"
+    flLogoXpath = "(//a[contains(@href, 'logo')])[1]"
+    emailFieldElement = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath(emailFieldID))
+    passFieldElement = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath(passFieldID))
+    loginButtonElement = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath(loginButtonXPath))
+    emailFieldElement.click()
+    emailFieldElement.clear()
+    emailFieldElement.send_keys("Mail Id")
+    passFieldElement.click()
+    passFieldElement.clear()
+    passFieldElement.send_keys("Password")
+    loginButtonElement.click()
+    WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath(flLogoXpath))
+    driver.save_screenshot("LoginPage.png")
+    print("\n    Time Executed - " + str(time.time() - start_time))
+    print("Login Succesfully!!")
+except Exception as e:
+    driver.save_screenshot("erro.png")
+    driver.quit()
+    raise
 # display.stop()
